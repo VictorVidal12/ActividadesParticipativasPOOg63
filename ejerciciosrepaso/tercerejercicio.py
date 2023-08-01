802808ad353517dfb4fba650451da2143e90be07
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-01 14:53:27 -0500

    Commit del tercer ejercicio de repaso.

--- ejerciciosrepaso/tercerejercicio.py
+++ ejerciciosrepaso/tercerejercicio.py
@@ -1,4 +1,12 @@
+import random
 
 
 def crear_lista_aleatoria():
-    pass
+    cantidad_numeros = int(input("Ingrese la cantidad entera de números a imprimir en la lista: "))
+    lista_aleatoria = []
+    for j in range(cantidad_numeros):
+        lista_aleatoria.append(random.randint(0, 10))
+    return lista_aleatoria
+
+
+print(crear_lista_aleatoria())
