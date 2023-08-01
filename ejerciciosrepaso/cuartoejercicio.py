8352a8eaf824eff904c21d762116cf87a32ff922
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-01 11:25:37 -0500

    Commit del cuarto ejercicio de repaso

---
+++ ejerciciosrepaso/cuartoejercicio.py
@@ -0,0 +1,10 @@
+
+
+def determinar_par_impar(numero: float):
+    if numero%2==0:
+        print(f"El número {numero} es par.")
+    else:
+        print(f"El número {numero} es impar.")
+    return
+
+determinar_par_impar(0)
