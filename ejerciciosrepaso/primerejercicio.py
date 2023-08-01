6140017c5f086d83dceb99a947392033c6983526
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-01 11:14:27 -0500

    Quitar los comentarios redundantes

--- ejerciciosrepaso/primerejercicio.py
+++ ejerciciosrepaso/primerejercicio.py
@@ -1,11 +1,6 @@
-""" En éste módulo crearé el primer ejercicio de los ejercicios de repaso para POO
-"""
+
 
 def imprimir_nombre_edad():
-    """
-    Éste método pide el nombre y la edad para después imprimirlos en la consola
-    :return: Nothing
-    """
     nombre = str(input("Ingresar nombre: "))
     edad = str(input("Ingresar edad: "))
     print(f"Su nombre es {nombre} y su edad es de {edad} años")
