560f6becb6589a822fd2b371ec28fc197b627af1
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-01 21:11:33 -0500

    Commit del décimotercer ejercicio de repaso.

---
+++ ejerciciosrepaso/decimotercerejercicio.py
@@ -0,0 +1,9 @@
+
+
+def operaciones_numeros():
+    numero_1 = float(input("Ingresar primer número: "))
+    numero_2 = float(input("Ingresar segundo número: "))
+    print(f"La suma entre los dos números es {numero_1 + numero_2}, la resta es {numero_1 - numero_2}, la multiplicación es {numero_1*numero_2} y la división es {numero_1/numero_2}")
+
+operaciones_numeros()
+
