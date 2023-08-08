0cf5b0c8eaa013aeb9f0d89f6849c7cfdd99f2ff
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-07 19:00:18 -0500

    Commit para implementar la prueba del método aplicar_cuota_manejo de la clase CuentaBancaria

--- Actividades Participativas/ActividadParticipativa3/probarcuentabancaria.py
+++ Actividades Participativas/ActividadParticipativa3/probarcuentabancaria.py
@@ -1,5 +1,6 @@
 from clases import CuentaBancaria
 
+
 if __name__ == "__main__":
     cuenta_1 = CuentaBancaria("1025760171", "Victor Manuel Vidal Becerra")
     cuenta_1.depositar(10000)
@@ -9,3 +10,6 @@ if __name__ == "__main__":
     retirar_2 = cuenta_1.retirar(10000)
     print(retirar_2)
     cuenta_1.mostrar_detalles()
+    cuenta_1.depositar(2000)
+    cuenta_1.aplicar_cuota_manejo()
+    print(cuenta_1.balance)
