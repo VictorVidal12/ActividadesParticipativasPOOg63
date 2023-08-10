d08a64f46d249a83723f1f12d9d4a85f96401f53
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-10 14:44:17 -0500

    Commit irrelevante

--- Actividades Participativas/ActividadParticipativa3/probarrectangulo.py
+++ Actividades Participativas/ActividadParticipativa3/probarrectangulo.py
@@ -3,7 +3,6 @@ from clases import Punto, Rectangulo
 if __name__ == "__main__":
     punto1 = Punto(3, 3)
     punto2 = Punto(8, 7)
-
     rectangulo = Rectangulo(punto1, punto2)
     print(rectangulo.lados())
     print(rectangulo.calcular_perimetro())
